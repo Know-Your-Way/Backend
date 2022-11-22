4d13332aad0d22df934759afdb7895504acec6eb
TheRick88 <avalex2810@gmail.com~>
2022-11-22 12:25:41 +0100

    landmarks deleted

--- src/api/landamark/routes/landamark.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * landamark router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::landamark.landamark');
--- src/api/landamark/services/landamark.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * landamark service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::landamark.landamark');
